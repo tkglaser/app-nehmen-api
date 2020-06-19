2b845f9feceff74290b5770e9717eed0edc464b8
Thomas Glaser <thomas@tkglaser.net>
2020-06-19 11:37:24 +0100

    Test

---
+++ Controllers/TestController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace app_nehmen_api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/test")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return $"Success {DateTime.Now.ToString()}";
+        }
+    }
+}
